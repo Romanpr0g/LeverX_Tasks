047268410fbc4699be7e7a54b037f43e6ebc1111
Romanpr0g <romankachanov3@gmail.com>
2024-03-05 15:40:07 +0300

    used json to detect circular references

--- jsTasks/memoization/memoization.js
+++ jsTasks/memoization/memoization.js
@@ -1,39 +1,20 @@
 const memoization = (originalFunction) => {
   const cache = new Map();
 
-  return (memoized = (arg) => {
+  return (arg) => {
     if (!cache.has(arg)) {
       const result = originalFunction(arg);
 
       if (result !== undefined && !Number.isNaN(result)) {
-        if (!isCyclic(result)) {
+        try {
+          JSON.stringify(result);
           cache.set(arg, result);
-        }
+        } catch {}
       }
     }
+
     return cache.get(arg);
-  });
-};
-
-const isCyclic = (obj) => {
-  const seenObjects = [];
-
-  const detect = (obj) => {
-    if (obj && typeof obj === "object") {
-      if (seenObjects.indexOf(obj) !== -1) {
-        return true;
-      }
-      seenObjects.push(obj);
-      for (const key in obj) {
-        if (obj.hasOwnProperty(key) && detect(obj[key])) {
-          return true;
-        }
-      }
-    }
-    return false;
   };
-
-  return detect(obj);
 };
 
 export default memoization;
